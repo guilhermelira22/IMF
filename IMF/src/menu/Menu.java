845f608b67e863ad339aac568aa354df0533f984
guilhermelira22 <guilhermelira22@gmail.com>
2024-12-11 22:00:55 +0000

    entities

---
+++ IMF/src/menu/Menu.java
@@ -0,0 +1,29 @@
+package menu;
+
+import entities.Mission;
+import exceptions.InvalidFileException;
+import simulation.Manual;
+
+import java.util.Scanner;
+
+public class Menu {
+
+    public Menu() {
+    }
+
+    public void mainMenu(Mission mission) throws InvalidFileException {
+        System.out.println("1. Iniciar Missão\n2. Sair");
+        String choice = new Scanner(System.in).nextLine();
+        if (choice.equals("1")) {
+            beginMission(mission);
+        }
+    }
+
+    public void beginMission(Mission mission) throws InvalidFileException {
+        System.out.println("Iniciar Missão");
+
+        Manual manual = new Manual(mission);
+        manual.start();
+    }
+
+}
